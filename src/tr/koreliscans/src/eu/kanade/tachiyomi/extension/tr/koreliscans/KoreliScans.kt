4507ba2b2dd0adc6d28fe605a54134a0069e1d5e
Metehan Şenyer <mthansnyr@gmail.com>
2025-10-04 16:20:11 +0300

    Import statements updated in KoreliScans.kt for Page and Document classes.

--- src/tr/koreliscans/src/eu/kanade/tachiyomi/extension/tr/koreliscans/KoreliScans.kt
+++ src/tr/koreliscans/src/eu/kanade/tachiyomi/extension/tr/koreliscans/KoreliScans.kt
@@ -1,6 +1,8 @@
 package eu.kanade.tachiyomi.extension.tr.koreliscans
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.source.model.Page
+import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
 
